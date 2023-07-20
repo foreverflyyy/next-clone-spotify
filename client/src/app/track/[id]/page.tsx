4cf64746f0dc9460dd5a7410b4d5af8af36e7776
Nikita <nikita.ushackow.02@gmail.com>
2023-07-20 17:42:07 +0200

    front: change idPage

--- client/src/app/track/[id]/page.tsx
+++ client/src/app/track/[id]/page.tsx
@@ -1,36 +1,17 @@
-"use client"
 import React from 'react';
 import {ITrack} from "@/models/track";
-import {useRouter} from "next/navigation";
-import MyButton from "@/components/UI/MyButton";
+import axios from "axios";
 
-interface Props {
-    params: {
-        id: string
-    }
+async function getTrackById(id: string) {
+    const response = await axios.get(`http://localhost:5000/track/${id}`);
+    return response.data;
 }
 
-const Page = ({params: {id}}: Props) => {
-
-    const router = useRouter();
-
-    const track: ITrack = {
-        "_id": "64b6cea987a5ec39938ff6f8",
-        "name": "HDMI 5",
-        "artist": "Nekit",
-        "text": "bla bla bla",
-        "listeners": 0,
-        "comments": []
-    }
+const Page = async ({params: {id}}: { params: { id: string }}) => {
+    const track: ITrack = await getTrackById(id);
 
     return (
         <div>
-            <MyButton
-                className={"font-medium text-xl py-5"}
-                onClick={() => router.push("/track")}
-            >
-                Back
-            </MyButton>
             <div className={"p-1"}>
                 <div className={"rounded-md border-2 border-gray-600 p-2 flex flex-col"}>
                     <h2 className={"font-medium text-xl"}>Track: {track.name}</h2>
@@ -39,6 +20,13 @@ const Page = ({params: {id}}: Props) => {
                         <h2>Text: {track.text}</h2>
                         <h2>Id: {id}</h2>
                     </div>
+                    <img
+                        src={`http://localhost:5000/${track.picture}`}
+                        width={50}
+                        height={50}
+                        alt="Picture track"
+                        className={"hover:scale-[1.1] duration-300"}
+                    />
                 </div>
             </div>
         </div>
